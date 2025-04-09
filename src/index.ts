6e64b0a503ba1e4a32a8fe988ec0e25541871d4b
nonick <87268971+nonick-mc@users.noreply.github.com>
2025-04-09 22:00:58 +0900

    feat: createDb関数の作成

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+﻿import { drizzle } from 'drizzle-orm/node-postgres';
+
+import * as auditLogSchema from './schema/audit-log';
+import * as authSchema from './schema/auth';
+import * as guildSchema from './schema/guild';
+import * as settingSchma from './schema/setting';
+
+export const createDb = () => {
+  return drizzle({
+    connection: {
+      // biome-ignore lint/style/noNonNullAssertion: <explanation>
+      connectionString: process.env.DATABASE_URL!,
+      ssl: false,
+    },
+    casing: 'snake_case',
+    schema: { ...authSchema, ...settingSchma, ...guildSchema, ...auditLogSchema },
+  });
+};
